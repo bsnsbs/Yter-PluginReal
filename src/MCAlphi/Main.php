aa88696b1ae7699a1aec2ee8bac9a0e2dcb33da8
bsnsbs <55895209+bsnsbs@users.noreply.github.com>
2020-08-02 02:38:31 +0100

    Add files via upload

---
+++ src/MCAlphi/Main.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Youtube
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+  public function onCommand(CommandSender $sender, Command $cmd, string $label.
+  array $args) : bool{
+
+switch(§cmd->getName()){
+case "yt":
+if($sender instanceof Player){
+$sender->sendMessage("§l§bDu brauchst 50 Abonnenten Für yter Rang");
+}
+break;
+}
+return true;
+}
+}
