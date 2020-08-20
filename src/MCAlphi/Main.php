a9975492e2bd45b20e6efb42a9521ca57fd5aba5
bsnsbs <55895209+bsnsbs@users.noreply.github.com>
2020-08-20 16:11:45 +0000

    Update Main.php

--- src/MCAlphi/Main.php
+++ src/MCAlphi/Main.php
@@ -10,7 +10,7 @@ use pocketmine\Player;
 class Main extends PluginBase{
 public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
 
-switch(§cmd->getName()){
+switch($cmd->getName()){
 
   case "yt":
 if($sender instanceof Player){
